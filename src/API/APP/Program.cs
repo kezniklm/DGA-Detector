749be0c5a0d51b8b0c807e24b2cea0d36b46136b
Matej Keznikl <xkezni01@vutbr.cz>
2024-03-11 17:34:52 +0100

    ADD: API done

--- src/API/APP/Program.cs
+++ src/API/APP/Program.cs
@@ -1,21 +1,140 @@
-// Copyright (c) .NET Foundation and contributors. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-//
+using APP.Config;
+using AutoMapper;
+using AutoMapper.Internal;
+using BL.Installers;
+using BL.Models.User;
+using Common.Extensions;
+using DAL.Entities.Interfaces;
+using DAL.Installers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+ConfigureCors(builder.Services);
 
-builder.Services.AddControllers();
+ConfigureOpenApiDocuments(builder.Services);
+
+ConfigureDependencies(builder.Services, builder.Configuration);
+
+ConfigureAutoMapper(builder.Services);
+
+ConfigureControllers(builder.Services);
 
 WebApplication app = builder.Build();
 
-// Configure the HTTP request pipeline.
+ValidateAutoMapperConfiguration(app.Services);
 
-app.UseHttpsRedirection();
+UseDevelopmentSettings(app);
 
-app.UseAuthorization();
+UseSecurityFeatures(app);
 
-app.MapControllers();
+UseRouting(app);
+
+UseEndpoints(app);
+
+UseOpenApi(app);
 
 app.Run();
+
+void ConfigureCors(IServiceCollection serviceCollection)
+{
+    serviceCollection.AddCors(options =>
+    {
+        options.AddDefaultPolicy(o =>
+            o.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod());
+    });
+}
+
+void ConfigureOpenApiDocuments(IServiceCollection serviceCollection)
+{
+    serviceCollection.AddEndpointsApiExplorer();
+    serviceCollection.AddOpenApiDocument();
+}
+
+void ConfigureDependencies(IServiceCollection serviceCollection, IConfiguration configuration)
+{
+    DbConfig dbConfig = GetDatabaseConfig(configuration);
+
+    serviceCollection.AddIdentityApiEndpoints<User>()
+        .AddMongoDbStores<User, UserRole, Guid>
+        (
+            dbConfig.ConnectionString, dbConfig.DatabaseName
+        )
+        .AddDefaultTokenProviders();
+
+    serviceCollection.AddInstaller<DalInstaller>(dbConfig.ConnectionString,dbConfig.DatabaseName);
+
+    serviceCollection.AddInstaller<BlInstaller>();
+}
+
+DbConfig GetDatabaseConfig(IConfiguration configuration)
+{
+    IConfigurationSection dbConfigSection = configuration.GetSection("DbConfig");
+
+    string connectionString = dbConfigSection["ConnectionString"] ??
+                              throw new ArgumentException("The connection string is missing");
+    string databaseName =
+        dbConfigSection["DatabaseName"] ?? throw new ArgumentException("The database name is missing");
+
+    return new DbConfig { ConnectionString = connectionString, DatabaseName = databaseName };
+}
+
+
+void ConfigureAutoMapper(IServiceCollection serviceCollection)
+{
+    serviceCollection.AddAutoMapper(typeof(IEntity), typeof(BlInstaller));
+}
+
+void ConfigureControllers(IServiceCollection serviceCollection)
+{
+    builder.Services.AddControllers();
+}
+
+void ValidateAutoMapperConfiguration(IServiceProvider serviceProvider)
+{
+    IMapper mapper = serviceProvider.GetRequiredService<IMapper>();
+    mapper.ConfigurationProvider.AssertConfigurationIsValid();
+}
+
+void UseDevelopmentSettings(WebApplication application)
+{
+    IWebHostEnvironment environment = application.Services.GetRequiredService<IWebHostEnvironment>();
+
+    if (environment.IsDevelopment())
+    {
+        application.UseDeveloperExceptionPage();
+    }
+}
+void UseSecurityFeatures(IApplicationBuilder application)
+{
+    application.UseCors();
+    application.UseHttpsRedirection();
+    MapIdentityEndpoints(app);
+    application.UseAuthorization();
+    application.UseAuthentication();
+}
+
+void MapIdentityEndpoints(WebApplication application)
+{
+    application.MapIdentityApi<User>().WithTags("Auth"); ;
+}
+
+void UseRouting(IApplicationBuilder application)
+{
+    application.UseRouting();
+}
+
+void UseEndpoints(WebApplication application)
+{
+    app.MapControllers();
+}
+
+void UseOpenApi(IApplicationBuilder application)
+{
+    application.UseOpenApi();
+    application.UseSwaggerUi();
+}
--- src/API/BL/Models/Result/ResultDetailModel.cs
+++ src/API/BL/Models/Result/ResultDetailModel.cs
@@ -6,5 +6,6 @@ public record ResultDetailModel : IModel
 {
     public required Guid Id { get; set; }
     public required string DomainName { get; set; }
+    public DateTime Detected { get; set; }
     public double Value { get; set; }
 }
