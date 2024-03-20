2beea070cfcdf64420ce3a14d44dec2746f67e91
Matej Keznikl <xkezni01@vutbr.cz>
2024-03-20 14:02:05 +0100

    FIX: windows problems with ValidatedDomains struct

--- src/Detector/include/Structures/ValidatedDomains.hpp
+++ src/Detector/include/Structures/ValidatedDomains.hpp
@@ -18,24 +18,63 @@
 
 #include <unordered_map>
 #include <string>
+#include <utility>
 
 /**
  * @struct ValidatedDomains
- * @brief Struct for storing validated domain-return code pairs.
+ * @brief Manages domain-return code pairs.
+ *
+ * This struct maintains a collection of domain names and their associated return codes,
+ * facilitating efficient access and management of this data.
  */
 struct ValidatedDomains
 {
-    /** Map storing domain-return code pairs */
+    /**
+     * @var domain_return_code_pairs_
+     * @brief Stores domain and return code pairs.
+     *
+     * This unordered map stores domains as keys and their associated return codes as values.
+     * It provides fast lookup of return codes based on domain names.
+     */
     std::unordered_map<std::string, int> domain_return_code_pairs_;
 
     /**
-     * @brief Constructor with parameters.
-     * @param domain_return_code_pairs The domain-return code pairs to initialize the struct.
+     * @brief Constructs a ValidatedDomains object with given domain-return code pairs.
+     * @param domain_return_code_pairs Map of domain-return code pairs for initialization.
+     * @note This constructor is noexcept, ensuring it does not throw any exceptions.
      */
-    ValidatedDomains(const std::unordered_map<std::string, int> domain_return_code_pairs) : domain_return_code_pairs_(domain_return_code_pairs) {}
+    ValidatedDomains(const std::unordered_map<std::string, int> &domain_return_code_pairs) noexcept
+        : domain_return_code_pairs_(domain_return_code_pairs) {}
+
+    /**
+     * @brief Default move constructor.
+     * @note Utilizes default noexcept behavior for efficient container operations.
+     */
+    ValidatedDomains(ValidatedDomains &&other) noexcept = default;
+
+    /**
+     * @brief Default copy constructor.
+     * @note Utilizes default noexcept behavior for safe exception handling.
+     */
+    ValidatedDomains(const ValidatedDomains &other) noexcept = default;
 
     /**
      * @brief Default constructor.
+     * Constructs an empty ValidatedDomains object.
      */
     ValidatedDomains() = default;
+
+    /**
+     * @brief Default move assignment operator.
+     * @return Reference to the modified ValidatedDomains object.
+     * @note Utilizes default noexcept behavior for efficient container operations.
+     */
+    ValidatedDomains &operator=(ValidatedDomains &&other) noexcept = default;
+
+    /**
+     * @brief Default copy assignment operator.
+     * @return Reference to the modified ValidatedDomains object.
+     * @note Utilizes default noexcept behavior for safe exception handling.
+     */
+    ValidatedDomains &operator=(const ValidatedDomains &other) noexcept = default;
 };
