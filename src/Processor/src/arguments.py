e3e95bff0374ccbf1e8eaf3eae9288fa719f7364
Matej Keznikl <matej.keznikl@gmail.com>
2024-03-22 11:36:39 +0100

    ADD: database uri and database name to arguments + add comments

--- src/Processor/src/arguments.py
+++ src/Processor/src/arguments.py
@@ -1,3 +1,21 @@
+"""
+ * @file Arguments.py
+ * @brief Handles parsing and storing of command line arguments for the application.
+ *
+ * This file contains the implementation of the Arguments class, which is responsible for parsing command-line arguments using the argparse library. It also loads configuration settings from a JSON file named 'appsettings.json' if available.
+ *
+ * The main functionalities of this file include:
+ * - Parsing command-line arguments using the argparse library.
+ * - Loading configuration settings from 'appsettings.json'.
+ * - Adding command-line arguments to the parser with defaults from the config file.
+ * - Checking for missing arguments and printing help message if any are missing.
+ *
+ * @version 1.0
+ * @date 2024-03-22
+ * @author Matej Keznikl (matej.keznikl@gmail.com)
+ * @copyright Copyright (c) 2024
+"""
+
 import argparse
 import json
 import sys
@@ -6,13 +24,33 @@ from src.return_codes import ReturnCodes
 
 
 class Arguments:
+    """
+    Handles parsing and storing of command line arguments for the application.
+
+    This class parses command-line arguments using the argparse library. It also loads configuration settings from a JSON file named 'appsettings.json' if available.
+
+    Attributes:
+        parser (argparse.ArgumentParser): An instance of the ArgumentParser class for defining command-line arguments.
+        config (dict): A dictionary containing configuration settings loaded from 'appsettings.json'.
+    """
+
     def __init__(self) -> None:
+        """
+        Initialize the Arguments class.
+
+        Initializes the ArgumentParser and loads configuration settings.
+        """
         self.parser = argparse.ArgumentParser(description="Application configuration")
         self.config = self.__load_config()
         self.__add_arguments()
 
     def __load_config(self) -> dict:
-        """Loads configuration from 'appsettings.json' if available."""
+        """
+        Loads configuration from 'appsettings.json' if available.
+
+        Returns:
+            dict: A dictionary containing configuration settings loaded from 'appsettings.json'.
+        """
         try:
             with open("appsettings.json", "r") as file:
                 return json.load(file)
@@ -23,7 +61,9 @@ class Arguments:
         return {}
 
     def __add_arguments(self) -> None:
-        """Adds command line arguments to the parser with defaults from the config file."""
+        """
+        Adds command line arguments to the parser with defaults from the config file.
+        """
         self.parser.add_argument(
             "-r",
             "--rabbitmq",
@@ -43,12 +83,21 @@ class Arguments:
         )
 
         self.parser.add_argument(
-            "-a",
-            "--api",
+            "-d",
+            "--database",
             type=str,
-            default=self.config.get("api"),
-            required="api" not in self.config,
-            help='Domain name for the API, e.g., "example.com". Required if not specified in appsettings.json',
+            default=self.config.get("database"),
+            required="database" not in self.config,
+            help="Connection string to the database, e.g., 'user:password@host:port/dbname'. Required if not specified in appsettings.json",
+        )
+
+        self.parser.add_argument(
+            "-n",
+            "--dbname",
+            type=str,
+            default=self.config.get("dbname"),
+            required="dbname" not in self.config,
+            help="Name of the database. Required if not specified in appsettings.json",
         )
 
         self.parser.add_argument(
@@ -59,8 +108,21 @@ class Arguments:
             help="Number of processing threads. Default is 1 if not specified in appsettings.json or command line.",
         )
 
+        self.parser.add_argument(
+            "-s",
+            "--queue-size",
+            type=int,
+            default=self.config.get("queue_size", 10),
+            help="Size of the queue between the RabbitMQ consumer and the extractor. Default is 10 if not specified in appsettings.json or command line.",
+        )
+
     def parse_args(self) -> argparse.Namespace:
-        """Parses the command line arguments and checks for missing arguments."""
+        """
+        Parses the command line arguments and checks for missing arguments.
+
+        Returns:
+            argparse.Namespace: An object containing parsed arguments.
+        """
         args = self.parser.parse_args()
 
         missing_args = [name for name, value in vars(args).items() if value is None]
