52bbc1a6996559e036c6227e7137ed8f48aa1f95
Matej Keznikl <matej.keznikl@gmail.com>
2024-03-22 11:33:11 +0100

    ADD: logging and comments to processor.py

--- src/Processor/processor.py
+++ src/Processor/processor.py
@@ -1,41 +1,88 @@
+"""
+ * @file processor.py
+ * @brief Handles message processing for the application.
+ *
+ * This file contains the implementation of the Processor class, which is responsible for managing message processing within the application. It initializes Extractors and RabbitMQConsumer, starts them, and provides a mechanism for graceful shutdown.
+ *
+ * The main functionalities of this file include:
+ * - Initializing Extractors and RabbitMQConsumer for message processing.
+ * - Starting Extractors and RabbitMQConsumer threads.
+ * - Entering a main processing loop until a shutdown event occurs.
+ * - Handling graceful shutdown upon user interruption (CTRL+C).
+ * - Joining Extractors threads upon shutdown.
+ *
+ * @version 1.0
+ * @date 2024-03-22
+ * @author Matej Keznikl (matej.keznikl@gmail.com)
+ * @copyright Copyright (c) 2024
+"""
+
 import queue
 import threading
 import time
+from typing import List
 
 from src.arguments import Arguments
 from src.extractor import Extractor
+from src.logger import Logger
 from src.rabbitmq_consumer import RabbitMQConsumer
 
 
 class Processor:
-    def __init__(self):
+    """
+    A class representing the main processing unit of the application.
+
+    This class initializes Extractors and RabbitMQConsumer to process messages.
+
+    Attributes:
+        shutdown_event (threading.Event): A threading event to signal shutdown.
+        message_queue (queue.Queue): A queue to store incoming messages.
+        extractors (List[Extractor]): A list of Extractor instances for processing messages.
+        consumer (RabbitMQConsumer): A RabbitMQConsumer instance for consuming messages.
+        logger (Logger): A logger instance for logging events.
+    """
+
+    def __init__(self) -> None:
+        """
+        Initialize the Processor class.
+
+        Initializes Extractors, RabbitMQConsumer, Logger, and other attributes.
+        """
         args = Arguments().parse_args()
         self.shutdown_event = threading.Event()
-        self.message_queue = queue.Queue(5)
-        self.extractors = [
-            Extractor(self.message_queue, self.shutdown_event)
+        self.message_queue = queue.Queue(args.queue_size)
+        self.extractors: List[Extractor] = [
+            Extractor(
+                self.message_queue, self.shutdown_event, args.database, args.dbname
+            )
             for _ in range(args.threads)
         ]
         self.consumer = RabbitMQConsumer(
             args.rabbitmq, args.queue, self.message_queue, self.shutdown_event
         )
+        self.logger = Logger().get_logger()
 
-    def run(self):
+    def run(self) -> None:
+        """
+        Start the processing loop.
+
+        Starts Extractors and RabbitMQConsumer, and enters the main processing loop.
+        """
         for extractor in self.extractors:
             extractor.start()
         self.consumer.start()
 
-        print("Application is running. Press CTRL+C to exit.\n")
+        self.logger.info("Application is running. Press CTRL+C to exit.\n")
         try:
             while not self.shutdown_event.is_set():
                 time.sleep(10)
         except KeyboardInterrupt:
-            print("Interrupted by user, initiating shutdown...\n")
+            self.logger.info("Interrupted by user, initiating shutdown...\n")
             self.shutdown_event.set()
 
         for extractor in self.extractors:
             extractor.join()
-        print("Processor has exited.")
+        self.logger.info("Processor has exited.")
 
 
 if __name__ == "__main__":
